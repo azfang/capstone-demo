7f71257a301ffa45fe8d0957f790d19771de228f
azfang <47017488+azfang@users.noreply.github.com>
2021-05-20 11:44:19 -0700

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ video.setAttribute('src', 'IHateThis.mp4');
 const URL =
         "data.json";
       document.addEventListener("DOMContentLoaded", () => {
-        //set up the IntersectionObserver to load more text if the footer is visible
+        //set up the IntersectionObserver to load more images if the footer is visible.
         //URL - https://gist.githubusercontent.com/prof3ssorSt3v3/1944e7ba7ffb62fe771c51764f7977a4/raw/c58a342ab149fbbb9bb19c94e278d64702833270/infinite.json
         let options = {
           root: null,
@@ -20,16 +20,41 @@ const URL =
         //an initial load of some data
         getData();
       });
+
       function handleIntersect(entries) {
-        if (entries[0].isIntersecting && pages >= 1) {
-          pages++;
-          if (pages === 2) {
-            video.setAttribute('src', 'IHateThis2.mp4');
-          }
+        if (entries[0].isIntersecting) {
           console.warn("something is intersecting with the viewport");
           getData();
+          pages++;
       }
     }
+
+      function handleLineIntersect(entries) {
+        if (entries[0].isIntersecting) {
+            video.setAttribute('src', 'IHateThis2.mp4');
+        }
+        else {
+          video.setAttribute('src', 'IHateThis.mp4');
+        }
+      }
+
+      function handleLine2Intersect(entries) {
+        if (entries[0].isIntersecting) {
+            video.setAttribute('src', 'IHateThis3.mp4');
+        }
+        else {
+          video.setAttribute('src', 'IHateThis.mp4');
+        }
+      }
+
+      function handleLine3Intersect(entries) {
+        if (entries[0].isIntersecting) {
+            video.setAttribute('src', 'painting_test.mp4');
+        }
+        else {
+          video.setAttribute('src', 'IHateThis.mp4');
+        }
+      }
     
     // function scrollFunction() {
     //   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
@@ -51,6 +76,14 @@ const URL =
           .then(response => response.json())
           .then(data => {
             // i++;
+            // data.items.forEach(item => {
+            //   let poetryText = document.createElement("div");
+            //   poetryText.classList.add("justify");
+            //   poetryText.id = `line-${pages}-${i}`;
+            //   poetryText.textContent = item.name;
+            //   main.appendChild(poetryText);
+            //   i++;
+            // });
             data.items.forEach(item => {
               let poetryText = document.createElement("div");
               poetryText.classList.add("justify");
@@ -58,10 +91,33 @@ const URL =
               poetryText.textContent = item.name;
               main.appendChild(poetryText);
               i++;
+
+              if (i === 7) {
+                const observer = new IntersectionObserver(handleLineIntersect, {
+                  root: null,
+                  rootMargins: "0px",
+                  threshold: 0.5
+                });
+                observer.observe(poetryText);
+              }
+
+              if (i === 14) {
+                const observer = new IntersectionObserver(handleLine2Intersect, {
+                  root: null,
+                  rootMargins: "0px",
+                  threshold: 0.5
+                });
+                observer.observe(poetryText);
+              }
+
+              if (i === 21) {
+                const observer = new IntersectionObserver(handleLine3Intersect, {
+                  root: null,
+                  rootMargins: "0px",
+                  threshold: 0.5
+                });
+                observer.observe(poetryText);
+              }
             });
           });
-      }
-      if (pages === 1 && i === 7) {
-        video.setAttribute('src', 'Waves_Scene1.mp4');
-        console.log("hi");
       }
